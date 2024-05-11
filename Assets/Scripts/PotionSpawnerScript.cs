1ed49ae671f267fa843a92438f7fc7392f028597
lukeslater0961 <luke.a.slater@outlook.com>
2024-05-12 00:37:09 +0200

    got a stable potion spawner just need to add the mead

--- Assets/Scripts/PotionSpawnerScript.cs
+++ Assets/Scripts/PotionSpawnerScript.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class PotionSpawnerScript : MonoBehaviour
 {
+    public EnemySpawner EnemySpawner;
+    public GameObject ShieldPotionprefab;
+    public GameObject Meadprefab;
 
+    public string[] Potions;
+    public int Potions_value;
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (EnemySpawner.gameStart == true)
+        {
+            Debug.Log("POTION SPAWNER");
+            SpawnRoutine();
+        }
+    }
+
+    void    SpawnRoutine()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-10f, 10f), 0.2f, Random.Range(-10f, 10f));
+        Potions_value = Random.Range(0, Potions.Length);
+        if (Potions_value == 1)
+            Instantiate(ShieldPotionprefab, spawnPos, ShieldPotionprefab.transform.rotation);
+        EnemySpawner.gameStart = false;
+    }
 }
